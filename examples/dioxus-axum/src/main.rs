12e2fc07526ff208cf43ca7f06d3795fdcc5ae0f
Daniëlle Huisman <danielle@huisman.me>
2025-06-26 18:37:49 +0200

    feat(shield-dioxus): add Shield for Dioxus (#125)

---
+++ examples/dioxus-axum/src/main.rs
@@ -0,0 +1,72 @@
+mod app;
+mod home;
+
+use crate::app::App;
+
+#[cfg(not(feature = "server"))]
+fn main() {
+    dioxus::launch(App);
+}
+
+#[cfg(feature = "server")]
+#[tokio::main]
+async fn main() {
+    use std::sync::Arc;
+
+    use axum::Router;
+    use dioxus::{
+        cli_config::fullstack_address_or_localhost,
+        prelude::{DioxusRouterExt, *},
+    };
+    use shield::{Shield, ShieldOptions};
+    use shield_dioxus_axum::ShieldLayer;
+    use shield_memory::MemoryStorage;
+    use shield_oidc::{Keycloak, OidcMethod};
+    use tokio::net::TcpListener;
+    use tower_sessions::{Expiry, MemoryStore, SessionManagerLayer, cookie::time::Duration};
+    use tracing::{Level, info};
+
+    // Initialize Dioxus
+    let addr = fullstack_address_or_localhost();
+    dioxus::logger::init(Level::DEBUG).unwrap();
+
+    // Initialize sessions
+    let session_store = MemoryStore::default();
+    let session_layer = SessionManagerLayer::new(session_store)
+        .with_secure(false)
+        .with_expiry(Expiry::OnInactivity(Duration::minutes(10)));
+
+    // Initialize Shield
+    let storage = MemoryStorage::new();
+    let shield = Shield::new(
+        storage.clone(),
+        vec![Arc::new(
+            OidcMethod::new(storage).with_providers([Keycloak::builder(
+                "keycloak",
+                "http://localhost:18080/realms/Shield",
+                "client1",
+            )
+            .client_secret("xcpQsaGbRILTljPtX4npjmYMBjKrariJ")
+            .redirect_url(format!(
+                "http://localhost:{}/api/auth/sign-in/callback/oidc/keycloak",
+                addr.port()
+            ))
+            .build()]),
+        )],
+        ShieldOptions::default(),
+    );
+    let shield_layer = ShieldLayer::new(shield.clone());
+
+    // Initialize router
+    let router = Router::new()
+        .serve_dioxus_application(ServeConfig::new().unwrap(), App)
+        .layer(shield_layer)
+        .layer(session_layer);
+
+    // Start app
+    info!("listening on http://{}", &addr);
+    let listener = TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, router.into_make_service())
+        .await
+        .unwrap();
+}
---
+++ packages/integrations/shield-dioxus/README.md
@@ -0,0 +1,13 @@
+<h1 align="center">Shield Dioxus</h1>
+
+[Dioxus](https://dioxuslabs.com/) integration for Shield.
+
+## Documentation
+
+See [the Shield book](https://shield.rustforweb.org/) for documentation.
+
+## Rust for Web
+
+The Shield project is part of [Rust for Web](https://github.com/RustForWeb).
+
+[Rust for Web](https://github.com/RustForWeb) creates and ports web libraries for Rust. All projects are free and open source.
