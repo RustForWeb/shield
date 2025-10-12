3f1eeefac43d86056c513056356dcd49e8060dd1
Daniëlle Huisman <danielle@huisman.me>
2025-10-12 13:49:17 +0200

    fix(react): use `exportFromIndex` Hey API option

--- packages/integrations/shield-react/src/index.ts
+++ packages/integrations/shield-react/src/index.ts
@@ -1,7 +1,6 @@
 import type { ActionForms } from './client/types.gen.js';
 
 export * from './client/index.js';
-export * from './client/@tanstack/react-query.gen.js';
 
 export type ReactStyleProps = {
     action: ActionForms;
